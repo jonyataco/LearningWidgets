5e1d1196a2be1e77d9a8e5f8586baffb475be25f
Jonathan Yataco <43523132+jonyataco@users.noreply.github.com>
2022-09-26 22:59:34 -0400

    Added first widget project
    Added first completed widget project from the course: Monthly

---
+++ Monthly/Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Jonathan Yataco on 9/26/22.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Monthly/MonthlyWidget/MonthlyWidget.swift
@@ -0,0 +1,117 @@
+//
+//  MonthlyWidget.swift
+//  MonthlyWidget
+//
+//  Created by Jonathan Yataco on 9/26/22.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> DayEntry {
+        DayEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (DayEntry) -> ()) {
+        let entry = DayEntry(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [DayEntry] = []
+
+        // Generate a timeline consisting of seven entries a day apart, starting from the current date.
+        let currentDate = Date()
+        for dayOffset in 0 ..< 7 {
+            let entryDate = Calendar.current.date(byAdding: .day, value: dayOffset, to: currentDate)!
+            let startOfDate = Calendar.current.startOfDay(for: entryDate)
+            let entry = DayEntry(date: startOfDate)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct DayEntry: TimelineEntry {
+    let date: Date
+}
+
+struct MonthlyWidgetEntryView : View {
+    var entry: DayEntry
+    let config: MonthConfig
+    
+    init(entry: DayEntry) {
+        self.entry = entry
+        self.config = MonthConfig.determineConfig(from: entry.date)
+    }
+
+    var body: some View {
+        ZStack {
+            ContainerRelativeShape()
+                .fill(config.backgroundColor.gradient)
+            
+            VStack {
+                HStack(spacing: 4) {
+                    Text(config.emojiText)
+                        .font(.title)
+                    Text(entry.date.weekdayDisplayFormat)
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .minimumScaleFactor(0.6)
+                        .foregroundColor(config.weekdayTextColor)
+                    Spacer()
+                }
+                
+                Text(entry.date.dayDisplayFormat)
+                    .font(.system(size: 80, weight: .heavy))
+                    .foregroundColor(config.dayTextColor)
+            }
+            .padding()
+        }
+    }
+}
+
+@main
+struct MonthlyWidget: Widget {
+    let kind: String = "MonthlyWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            MonthlyWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("Monthly Style Widget")
+        .description("The theme of the widget changes based on month.")
+        .supportedFamilies([.systemSmall])
+    }
+}
+
+struct MonthlyWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        MonthlyWidgetEntryView(entry: DayEntry(date: dateToDisplay(month: 3, day: 22)))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+    
+    static func dateToDisplay(month: Int, day: Int) -> Date {
+        let components = DateComponents(
+            calendar: Calendar.current,
+            year: 2022,
+            month: month,
+            day: day
+        )
+        
+        return Calendar.current.date(from: components)!
+    }
+}
+
+extension Date {
+    var weekdayDisplayFormat: String {
+        self.formatted(.dateTime.weekday(.wide))
+    }
+    
+    var dayDisplayFormat: String {
+        self.formatted(.dateTime.day())
+    }
+}
